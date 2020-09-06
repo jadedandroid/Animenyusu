ee021b38654e6db011698fbfeea2bea2e2164516
Alex E(She/Her) <eisea904@newschool.edu>
2020-09-06 00:09:27 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Anime
+yu_yu_hakusho = Anime.new(name: "Yu Yu Hakusho", description: "Shounen")
+dragon_ball = Anime.new(name: "Dragon Ball", description: "Shounen")
+sailor_moon = Anime.new(name: "Sailor Moon", description: "Shoujo")
+gundam = Anime.new(name: "Gundam", description: "Mecha")
+wolfs_rain = Anime.new(name: "Wolf's Rain", description: "drama")
+code_geas = Anime.new(name: "Code Geas", description: "drama")
+evangalion = Anime.new(name: "Evangalion", description: "Mecha")
+highschool_of_the_dead = Anime.new(name: "Highschool of the Dead", description: "Post-Apocolyptic")
+yuri_on_ice = Anime.new(name: "Yuri on Ice", description: "Romance")
+fairytail = Anime.new(name: "Fairytail", description: "Shonen")
+the_seven_deadly_sins = Anime.new(name: "The Seven Deadly Sins", description: "Fantasy")
+
+
