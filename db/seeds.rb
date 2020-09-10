b5e86b56c1b9d8ca173ebeb0adc6d0123d8e4d06
jadedandroid <lacroixjaded@gmail.com>
2020-09-10 11:24:11 -0400

    idrca

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,12 @@ sailor_moon = Anime.create(name: "Sailor Moon", description: "Shoujo")
 gundam = Anime.create(name: "Gundam", description: "Mecha")
 wolfs_rain = Anime.create(name: "Wolf's Rain", description: "Drama")
 code_geas = Anime.create(name: "Code Geas", description: "Drama")
-evangalion = Anime.create(name: "Evangalion", description: "Mecha")
-highschool_of_the_dead = Anime.create(name: "Highschool of the Dead", description: "Post-Apocolyptic/Fan Service")
-yuri_on_ice = Anime.create(name: "Yuri on Ice", description: "Romance")
-fairytail = Anime.create(name: "Fairytail", description: "Shonen")
-the_seven_deadly_sins = Anime.create(name: "The Seven Deadly Sins", description: "Fantasy")
+evangalion = Anime.create(name: "Evangalion", description: "Mecha: ifteen years after a global cataclysm known as the Second Impact, teenager Shinji Ikari is summoned to the futuristic city of Tokyo-3 by his estranged father Gendo Ikari, director of the special paramilitary force Nerv. ... In his second battle, Shinji destroys an Angel but runs away afterwards, distraught.")
+highschool_of_the_dead = Anime.create(name: "Highschool of the Dead", description: "Post-Apocolyptic, he story follows a group of high school students, the high school's nurse, and a young girl as they fight their way to safety through the deadly streets of Japan during a worldwide catastrophic event known as the Outbreak")
+yuri_on_ice = Anime.create(name: "Yuri on Ice", description: "Romance: Japanese figure skater Yuri Katsuki faces a crushing defeat during the Grand Prix finals and heads home, unsure of whether or not he wishes to continue his skating career. After a video of Yuri mimicking Russian figure skater Victor Nikiforov's routine goes viral, Victor decided to become Yuri's coach, much to the dismay of his coach, his fans, and his fellow Russian skater Yuri Plisetsky.
+After a disgrace in the Grand Prix Final, Japanese figure skater Katsuki Yuri seeks to retire. Victor Nikiforov, Russian world champion in figure skating is known for his surprises but is feeling stuck when he realises he has no more surprises left. When Yuri dances to Victor's most famous routine 'Stay Close to Me' the video is posted online and is seen by Victor. In this moment the Russian world champion decides to move to Japan and become Yuri's coach. Over the course of various competitions, the two form a powerful bond and see that each other was all they needed to accomplish what both had hoped for. Love.")
+fairytail = Anime.create(name: "Fairytail", description: "Shonen: Lucy Heartfilia, a traveling Mage, meets Natsu Dragneel, a Mage looking for his foster parent, a Dragon named Igneel, with his best friend, Happy, a blue, talking and flying cat. Shortly after their meeting, Lucy is abducted by Bora of Prominence, who was posing as Salamander of Fairy Tail, to be sold as a slave. Natsu rescues her and reveals that he is the real Salamander of Fairy Tail and has the skills of a Dragon Slayer, a form of Lost Magic. He offers her membership into the guild, which she gladly accepts. They, along with the armored Mage Erza Scarlet, Ice-Make Mage Gray Fullbuster, and Happy, become a team performing various missions offered to the Fairy Tail Guild.")
+the_seven_deadly_sins = Anime.create(name: "The Seven Deadly Sins", description: "Fantasy: he Seven Deadly Sins were the strongest and cruelest chivalric order in the kingdom, formed by seven brutal criminals who carved symbols of seven beasts into their bodies. Ten years ago, they were accused of plotting to overthrow the kingdom, and disintegrated after all the Holy Knights launched a general offensive against them.")
 
 #User
 alex = User.create(username: "Alex", password_digest: "password")
@@ -33,3 +34,13 @@ sailor_moon_blog = Post.create(title: "Neptune and Uranus", content: "true love"
 
 #Tag
 shounen = Tag.create(genre: "Shounen")
+drama = Tag.create(genre: "drama")
+mecha = Tag.create(genre: "Mecha")
+romance = Tag.create(genre: "Romance")
+post_apocolyptic = Tag.create(genre: "Post Apocolyptic")
+shoujo = Tag.create(genre: "shoujo")
+fantasy = Tag.create(genre: "fantasy")
+magical = Tag.create(genre: "Magical")
+action = Tag.create(genre: "Action")
+superpower = Tag.create(genre: "superpower")
+
